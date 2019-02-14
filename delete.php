f8cec20d0131f329674902e2d222c3abb1d1323a
George <george@proto366.haagahelia.amk>
2019-02-14 13:18:00 +0200

    fixed delete

--- delete.php
+++ delete.php
@@ -4,8 +4,7 @@ if($_SESSION['username'] == 'username') {
  include "connect.php";
  print("<html>");
  $conn = connect_db();
- $sql = "DELETE FROM ADDRESSES
-WHERE ID = '" . $_POST['id'] .;";
+ $sql = "DELETE FROM Addresses WHERE id=" . $_GET['id'];
  // print $sql;
  $result = mysqli_query($conn, $sql);
  print("Delete completed.");
@@ -13,4 +12,5 @@ WHERE ID = '" . $_POST['id'] .;";
  mysqli_close($conn);
 }
 ?>
+
 
